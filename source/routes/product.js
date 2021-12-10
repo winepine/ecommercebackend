d282c65682083e4955f9bf2395c0acdb569840c2
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2021-12-10 16:50:17 +0500

    changes made

--- source/routes/product.js
+++ source/routes/product.js
@@ -1,7 +1,7 @@
 const express= require('express');
 
 const { requireSignin, verifyAdmin} = require('../controllers/admin/user');
-const {createProduct}= require('../controllers/product');
+const { createProduct } = require('../controllers/product');
 
 const router= express.Router();
 

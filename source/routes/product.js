171cb3886d016c184412e68fb8f9bc7379937377
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2021-12-13 20:45:12 +0500

    pictures updated

--- source/routes/product.js
+++ source/routes/product.js
@@ -2,10 +2,26 @@ const express= require('express');
 
 const { requireSignin, verifyAdmin} = require('../controllers/admin/user');
 const { createProduct } = require('../controllers/product');
-
+const multer=require('multer');
+const shortid= require('shortid');
+const path=require('path');
 const router= express.Router();
+//multer is always configured on the router 
 
-router.post('/product/create', requireSignin, verifyAdmin, createProduct);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/Users/user/Desktop/shafaqbasit/ecommercebackend/source/pictures')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      //cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.originalname);
+    }
+  })
+  const picture=multer({storage: storage});
+
+router.post('/product/create', requireSignin, verifyAdmin,picture.array('image'), createProduct);
 router.get('/product/products')
 
 module.exports=router;
